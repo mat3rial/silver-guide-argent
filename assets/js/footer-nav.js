bd595266fbc9911f17f7cfd0a433c1ba3e455c60
evansavage <esav678@gmail.com>
2020-06-28 23:35:21 -0400

    Updated guide footer nav logic

--- assets/js/footer-nav.js
+++ assets/js/footer-nav.js
@@ -7,15 +7,16 @@ $( document ).ready(function() {
     var section_splits = section.split("_").map(function(x) {
       return parseInt(x, 10)
     })
+    console.log(section, section_splits)
     // if (section_splits.length == 1) {
     //   $('section-nav').children().eq();
     // }
-    if (section_splits.length > 1) {
+    if (section_splits[0] > 1) {
       $('.section-nav').css('border-bottom', '1px solid black');
     }
-    $('.section-nav').children().eq(section_splits[0]-1).css('color', site_red);
+    $('.section-nav').children().eq(section_splits[0]-1).find('a').css('color', site_red);
     if (section_splits[0] > 1) {
-      $('.subsection-nav').children().eq(section_splits[1]-1).css('color', site_red);
+      $('.subsection-nav').children().eq(section_splits[1]-1).find('a').css('color', site_red);
     }
 
     // if (section_splits.length == 3) {
