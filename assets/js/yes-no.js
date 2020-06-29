f980f98440a55cbe7862f64f4742c5e3ec84cea1
evansavage <esav678@gmail.com>
2020-06-28 23:34:12 -0400

    Created navigation yes/no button

--- assets/js/yes-no.js
+++ assets/js/yes-no.js
@@ -1,6 +1,7 @@
 const color = "#F92403";
 // const right_color = "green";
-
+var guideButton = $('.nav-yes-no').find('.guide-button');
+var _href = guideButton.attr('href');
 var answered = 0;
 $( document ).ready(function() {
   $('.choices div').hover(function(){
@@ -8,12 +9,24 @@ $( document ).ready(function() {
       $(this).toggleClass('hover');
     }
   });
-
+  // $('.choices div').on('click', function() {
+  //
+  // });
   $('.right').on('click', function() {
     // $(this).css({
     //   'background-color': color,
     //   'color': "white"
     // });
+    if ($(this).parents('.outer-yes-no-wrapper').hasClass('nav-yes-no')) {
+
+      // var _href = guideButton.attr('href');
+      guideButton.slideDown('fast');
+      // guideButton.attr('href',function(i,str) {
+      //   return str + $(this).attr('data-link');
+      // });
+      guideButton.attr('href', _href + $(this).attr('data-link'));
+      console.log($(this).attr('data-link'));
+    }
     $(this).addClass('active');
     $(this).parent().addClass('used');
     $(this).removeClass('hover');
@@ -26,6 +39,16 @@ $( document ).ready(function() {
     answered = 1
   });
   $('.wrong').on('click', function() {
+    if ($(this).parents('.outer-yes-no-wrapper').hasClass('nav-yes-no')) {
+      var guideButton = $(this).parents('.nav-yes-no').find('.guide-button');
+      // var _href = guideButton.attr('href');
+      guideButton.slideDown('fast');
+      // guideButton.attr('href',function(i,str) {
+      //   return str + $(this).attr('data-link');
+      // });
+      guideButton.attr('href', _href + $(this).attr('data-link'));
+      console.log($(this).attr('data-link'));
+    }
     $(this).addClass('active');
     $(this).parent().addClass('used');
     $(this).removeClass('hover');
